cf223416eb220e467e882ea4df1a4b98ed8f34c0
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-03-24 14:50:40 +0100

    create benchmarking version of quick sort, with docs

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -37,11 +37,46 @@ impl QuickSort {
 
         l
     }
+
+    fn quick_sort_with_benchmark<T: Ord + Copy>(slice: &mut [T], benchmark: &mut impl Benchmark) {
+        if slice.len() <= 1 {
+            return;
+        }
+
+        let pivot_index = QuickSort::partition_with_benchmark(slice, benchmark);
+
+        QuickSort::quick_sort_with_benchmark(&mut slice[..pivot_index], benchmark);
+
+        if pivot_index < slice.len() - 1 {
+            QuickSort::quick_sort_with_benchmark(&mut slice[pivot_index + 1..], benchmark);
+        }
+    }
+
+    fn partition_with_benchmark<T: Ord + Copy>(slice: &mut [T], benchmark: &mut impl Benchmark) -> usize {
+        let pivot = slice[slice.len() - 1];
+
+        let mut l = 0;
+
+        for r in 0..slice.len() - 1 {
+            benchmark.add_cmp();
+            if slice[r] < pivot {
+                benchmark.add_swap();
+                slice.swap(l, r);
+                l += 1;
+            }
+        }
+
+        benchmark.add_swap();
+        slice.swap(l, slice.len() - 1);
+
+        l
+    }
 }
 
 impl Sorter for QuickSort {
     /// Quick sort implementation working on types implementing
     /// `Ord` + `Copy` so it's mostly usefull for primitive types.
+    /// It uses Lomuto partition scheme choosing last item as pivot.
     ///
     /// Examples:
     /// ```
@@ -56,6 +91,33 @@ impl Sorter for QuickSort {
     /// ```
     fn sort<T: Ord + Copy>(slice: &mut [T]) {
         QuickSort::quick_sort(slice);
+    }
+}
+
+impl BenchmarkingSorter for QuickSort {
+    /// Quick sort implementation with additional benchmarking capabilities.
+    ///
+    /// Examples:
+    /// ```
+    /// use algorithms::prelude::*;
+    /// use algorithms::benchmarking::StandardBenchmarker;
+    ///
+    /// let mut benchmarker = StandardBenchmarker::default();
+    /// let mut slice = [1, 8, 2, 3, 9, 5];
+    ///
+    /// QuickSort::sort_with_benchmark(&mut slice, &mut benchmarker);
+    ///
+    /// let stats = benchmarker.get_stats();
+    ///
+    /// assert_eq!(9, stats.comparisons);
+    /// assert_eq!(10, stats.swaps);
+    /// ```
+    fn sort_with_benchmark<T: Ord + Copy>(slice: &mut [T], benchmark: &mut impl Benchmark) {
+        benchmark.start_timer();
+
+        QuickSort::quick_sort_with_benchmark(slice, benchmark);
+
+        benchmark.stop_timer();
     }
 }
 
