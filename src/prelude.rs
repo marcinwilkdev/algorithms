29849dcc9049766a85150711009c38240c15064a
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-03-24 13:48:03 +0100

    add documenation to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,8 +1,15 @@
+//! Convenience re-export of common members and declaration of
+//! sorting traits.
+
 pub use crate::benchmarking::Benchmark;
+
+/// Trait for sorting algorithms.
 
 pub trait Sorter {
     fn sort<T: Ord + Copy>(slice: &mut [T]);
 }
+
+/// Trait for sorting algorithms with benchmarking capabilities.
 
 pub trait BenchmarkingSorter {
     fn sort_with_benchmark<T: Ord + Copy>(slice: &mut [T], benchmark: &mut impl Benchmark);
